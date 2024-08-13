3af7645b5234ee5bc45ebfb1d5de495346fe44cc
Lakshay <lakshaygoyal201@gmail.com>
2024-08-13 18:07:29 +0530

    add the real data in website

--- dist/index.js
+++ dist/index.js
@@ -25,3 +25,33 @@ window.onload = function () {
     }
   }, 1500);
 };
+
+const words = ["Goyal", "Portfolio"];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function type() {
+  currentWord = words[i];
+  if (isDeleting) {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j-1);
+    j--;
+    if (j == 0) {
+      isDeleting = false;
+      i++;
+      if (i == words.length) {
+        i = 0;
+      }
+    }
+  } else {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j+1);
+    j++;
+    if (j == currentWord.length) {
+      isDeleting = true;
+    }
+  }
+  setTimeout(type, 200);
+}
+
+type();
